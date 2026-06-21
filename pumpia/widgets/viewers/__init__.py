701d959a7ee55702b0193a87fdf1abff562c2ed5
Zack <150851679+zravetz@users.noreply.github.com>
2026-06-21 21:44:56 +0100

    Changes to be committed: 	new file:   pumpia/widgets/viewers/__init__.py 	renamed:    pumpia/widgets/viewers.py -> pumpia/widgets/viewers/viewers.py

---
+++ pumpia/widgets/viewers/__init__.py
@@ -0,0 +1,8 @@
+from pumpia.widgets.viewers.viewers import (Viewer,
+                                            BaseViewer,
+                                            ArrayViewer,
+                                            DicomViewer,
+                                            MonochromeViewer,
+                                            MonochromeDicomViewer,
+                                            ManualROIType,
+                                            MouseOptionsType)
